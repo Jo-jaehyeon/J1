72210e11b54a298fbdf4ee3959ca50343a1187fb
Jo-Jaehyeon <jerryjo0906@gmail.com>
2026-04-13 01:28:57 +0900

    [feature] Login UI 초안

---
+++ Source/J1/UI/Login/J1LoginWidget.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UI/Login/J1LoginWidget.h"
+#include "Components/EditableTextBox.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+void UJ1LoginWidget::NativeConstruct()
+{
+	if (Input_ID)
+		Input_ID->OnTextCommitted.AddDynamic(this, &UJ1LoginWidget::OnInputCommitted);
+
+	if (Input_PW)
+		Input_PW->OnTextCommitted.AddDynamic(this, &UJ1LoginWidget::OnInputCommitted);
+
+	if (Tab_Login)  Tab_Login->OnClicked.AddDynamic(this, &UJ1LoginWidget::OnTabLogin);
+	if (Tab_Join)   Tab_Join->OnClicked.AddDynamic(this, &UJ1LoginWidget::OnTabJoin);
+
+}
+
+void UJ1LoginWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+// ════════════════════════════════════
+//  위젯 바인딩 함수
+// ════════════════════════════════════
+void UJ1LoginWidget::OnInputCommitted(const FText& Text, ETextCommit::Type CommitMethod)
+{
+	if (CommitMethod == ETextCommit::Type::OnEnter)
+	{
+		OnTabLogin();
+	}
+}
+
+void UJ1LoginWidget::OnTabLogin()
+{
+	FString id = Input_ID->GetText().ToString();
+	FString pw = Input_PW->GetText().ToString();
+
+	Txt_Warning->SetVisibility(ESlateVisibility::Visible);
+	if(id.IsEmpty())			// ID란이 비어있다면
+	{
+		Txt_Warning->SetText(FText::FromString(TEXT("Please input ID")));
+	}
+	else if(pw.IsEmpty())		// PW란이 비어있다면
+	{
+		Txt_Warning->SetText(FText::FromString(TEXT("Please input password")));
+	}
+	else
+	{
+		Txt_Warning->SetVisibility(ESlateVisibility::Hidden);
+		UE_LOG(LogTemp, Log, TEXT("Input id : %s, pw : %s"), *id, *pw);
+		
+		// TODO : Login Packet 발송
+	}
+}
+
+void UJ1LoginWidget::OnTabJoin()
+{
+}
+
---
+++ Source/J1/UI/Login/J1LoginWidget.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "J1LoginWidget.generated.h"
+
+/*
+ *      Forward Declaration
+ */
+class UTextBlock;
+class UEditableTextBox;
+class UButton;
+
+UCLASS()
+class J1_API UJ1LoginWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+    virtual void NativeConstruct() override;
+    virtual void NativeDestruct() override;
+
+private:
+    // ════════════════════════════════════
+    //  위젯 바인딩 함수
+    // ════════════════════════════════════
+    UFUNCTION()
+    void OnInputCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+
+    UFUNCTION() void OnTabLogin();
+    UFUNCTION() void OnTabJoin();
+
+public:
+    // ════════════════════════════════════
+    //  UMG 바인딩
+    // ════════════════════════════════════
+    UPROPERTY(meta = (BindWidget))
+    UTextBlock* Txt_Warning;
+
+    UPROPERTY(meta = (BindWidget))
+    UEditableTextBox* Input_ID;
+
+    UPROPERTY(meta = (BindWidget))
+    UEditableTextBox* Input_PW;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tab_Login;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tab_Join;
+};

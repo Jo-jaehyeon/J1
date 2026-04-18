3cfb7f02aaf8fba9654f78811ecad779e28e14ef
Jo-Jaehyeon <jerryjo0906@gmail.com>
2026-04-18 19:25:46 +0900

    [refactor] 다양한 enum 사용을 위한 파일 구조 변경

---
+++ Source/J1/J1EnumTypes.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class ELoginMode : uint8
+{
+    Login,
+    CheckId,
+    SignUp
+};
+
+UENUM(BlueprintType)
+enum class EChatChannel : uint8
+{
+    All      UMETA(DisplayName = "일반"),
+    Party    UMETA(DisplayName = "파티"),
+    Guild    UMETA(DisplayName = "길드"),
+    Whisper  UMETA(DisplayName = "귓속말"),
+    System   UMETA(DisplayName = "시스템"),
+};

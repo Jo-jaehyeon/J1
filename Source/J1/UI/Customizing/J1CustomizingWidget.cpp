71fcc3a5545d3f213bb2a49667f60d06c6c2544c
Jo-Jaehyeon <jerryjo0906@gmail.com>
2026-08-03 01:35:50 +0900

    [feat] Customize UI

---
+++ Source/J1/UI/Customizing/J1CustomizingWidget.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright © 2026 Jerry. All rights reserved.
+
+#include "UI/Customizing/J1CustomizingWidget.h"
+
+void UJ1CustomizingWidget::NativeConstruct()
+{
+}
+
+void UJ1CustomizingWidget::ApplyClass(ECharacterClass NewClass)
+{
+}
+
+FCharacterSkinBaseData* UJ1CustomizingWidget::FindClassData(ECharacterClass InClass) const
+{
+	return nullptr;
+}
+
+bool UJ1CustomizingWidget::IsNicknameValid(const FString& Nickname) const
+{
+	return false;
+}
+
+void UJ1CustomizingWidget::OnSkinChanged(ESkinSlot SkinSlot, FSkinEntry NewSkin)
+{
+}
+
+void UJ1CustomizingWidget::OnConfirmClicked()
+{
+}
+void UJ1CustomizingWidget::OnBackClicked()
+{
+}
